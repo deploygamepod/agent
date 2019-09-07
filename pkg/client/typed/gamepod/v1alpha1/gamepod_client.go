4624f5b4268fdee032b3bfcea839b029fc7a3b11
Keith Mitchell <keith.mitchell@live.ca>
2019-09-07 08:19:37 +0100

    Updated clientset to use correct Kubernetes version 1.13.4

--- pkg/client/typed/gamepod/v1alpha1/gamepod_client.go
+++ pkg/client/typed/gamepod/v1alpha1/gamepod_client.go
@@ -21,6 +21,7 @@ package v1alpha1
 import (
 	v1alpha1 "gamepod.cc/agent/pkg/apis/gamepod/v1alpha1"
 	"gamepod.cc/agent/pkg/client/scheme"
+	serializer "k8s.io/apimachinery/pkg/runtime/serializer"
 	rest "k8s.io/client-go/rest"
 )
 
@@ -70,7 +71,7 @@ func setConfigDefaults(config *rest.Config) error {
 	gv := v1alpha1.SchemeGroupVersion
 	config.GroupVersion = &gv
 	config.APIPath = "/apis"
-	config.NegotiatedSerializer = scheme.Codecs.WithoutConversion()
+	config.NegotiatedSerializer = serializer.DirectCodecFactory{CodecFactory: scheme.Codecs}
 
 	if config.UserAgent == "" {
 		config.UserAgent = rest.DefaultKubernetesUserAgent()
